5afa1e0d2aea7682252793518ab9e0179bb60019
mujiu555@GitHub <1310769510@qq.com>
2026-04-14 04:38:52 +0800

    feat: quiz 7

--- flake.nix
+++ flake.nix
@@ -2,42 +2,52 @@
   description = "Flake for L99 Development";
 
   inputs = {
-    nixpkgs.url = "https://mirrors.ustc.edu.cn/nix-channels/nixos-unstable/nixexprs.tar.xz";
+    nixpkgs.url = "git+https://mirrors.nju.edu.cn/git/nixpkgs.git?ref=nixos-25.11&shallow=1";
   };
 
-  outputs = { self , nixpkgs ,... }: let
-    # system should match the system you are running on
-    system = "x86_64-linux";
-  in {
-    devShells."${system}".default = let
-      pkgs = import nixpkgs { inherit system; };
-    in pkgs.mkShell {
-      packages = with pkgs; [
-        (sbcl.withPackages (ps: with ps; [
-          quicklisp-starter
-          quicklisp-stats
-          linedit
-          slynk
-          slynk-macrostep
-          slynk-named-readtables
-        ]))
-      ];
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      # system should match the system you are running on
+      system = "x86_64-linux";
+    in
+    {
+      devShells."${system}".default =
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.mkShell {
+          packages = with pkgs; [
+            (sbcl.withPackages (
+              ps: with ps; [
+                quicklisp-starter
+                quicklisp-stats
+                linedit
+                slynk
+                slynk-macrostep
+                slynk-named-readtables
+                agnostic-lizard
+              ]
+            ))
+          ];
 
-      shellHook = ''
-          export SHELL="/run/current-system/sw/bin/bash" ;
-          export shell="/run/current-system/sw/bin/bash" ;
-      '';
+          shellHook = ''
+            export SHELL="/run/current-system/sw/bin/bash" ;
+            export shell="/run/current-system/sw/bin/bash" ;
+          '';
+        };
+      packages."${system}".default =
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.runCommand "vm" {
+          buildInputs = with pkgs; [
+            sbcl
+          ];
+          nativeBuildInputs = with pkgs; [
+            makeWrapper
+          ];
+        } "";
+
     };
-    packages."${system}".default = let
-      pkgs = import nixpkgs { inherit system; };
-    in pkgs.runCommand "vm" {
-      buildInputs = with pkgs; [
-          sbcl
-      ];
-      nativeBuildInputs = with pkgs; [
-        makeWrapper
-      ];
-    } ''
-    '';
-  };
 }
